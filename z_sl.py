b3592b6b6082e362ef1a5dfbe8862829dd91fd66
AHMerrill <52061142+AHMerrill@users.noreply.github.com>
2025-10-27 02:13:44 -0500

    Update z_sl.py

--- z_sl.py
+++ z_sl.py
@@ -30,6 +30,11 @@ try:
 except LookupError:
     nltk.download('punkt', quiet=True)
 
+try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab', quiet=True)
+
 # ------------------------------------------------------------
 # CONFIGURATION
 # ------------------------------------------------------------
